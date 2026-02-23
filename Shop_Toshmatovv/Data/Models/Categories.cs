7fbc7b46f11bbae38b645fb097e019d40d688752
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-02-23 17:49:13 +0500

    Разработанные модели данных.  Тошматов

---
+++ Shop_Toshmatovv/Data/Models/Categories.cs
@@ -0,0 +1,13 @@
+﻿namespace Shop_Toshmatovv.Data.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Items> Items { get; set; }
+    }
+}

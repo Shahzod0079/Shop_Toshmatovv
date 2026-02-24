7b8c9f145780533530fb3b6efef56d1fd0bb4115
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-02-24 12:21:51 +0500

    Получение данных о категориях. Тошматов

---
+++ Shop_Toshmatovv/Data/DataBase/DBCategory.cs
@@ -0,0 +1,37 @@
+﻿using MySql.Data.MySqlClient;
+using Shop_Toshmatovv.Data.Common;
+using Shop_Toshmatovv.Data.Interfaces;
+using Shop_Toshmatovv.Data.Models;
+using System.Collections.Generic;
+
+namespace Shop_Toshmatovv.Data.DataBase
+{
+    public class DBCategory : ICategorys
+    {
+        public IEnumerable<Categories> AllCategories
+        {
+            get
+            {
+                List<Categories> categories = new List<Categories>();
+
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+
+                MySqlDataReader CategorysData = Connection.MySqlQuery("SELECT * FROM Shop.Categorys ORDER BY `Name`;", MySqlConnection);
+
+                while (CategorysData.Read())
+                {
+
+                    categories.Add(new Categories()
+                    {
+                        Id = CategorysData.IsDBNull(0) ? -1 : CategorysData.GetInt32(0),
+                        Name = CategorysData.IsDBNull(1) ? null : CategorysData.GetString(1),
+                        Description = CategorysData.IsDBNull(2) ? null : CategorysData.GetString(2)
+                    });
+                }
+
+                // возвращаем список категорий
+                return categories;
+            }
+        }
+    }
+}

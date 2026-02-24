5e5b4c02cf2609d6abeb215af74fcab01be24a22
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-02-24 12:24:07 +0500

    Получение данных о предметах.  Тошматов

---
+++ Shop_Toshmatovv/Data/DataBase/DBItems.cs
@@ -0,0 +1,44 @@
+﻿using Shop_Toshmatovv.Data.Common;
+using Shop_Toshmatovv.Data.Interfaces;
+using Shop_Toshmatovv.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using MySql.Data.MySqlClient;
+
+namespace Shop_Toshmatovv.Data.DataBase
+{
+    public class DBItems : IItems
+    {
+        public IEnumerable<Categories> Categories = new DBCategory().AllCategories;
+
+        public IEnumerable<Items> AllItems
+        {
+            get
+            {
+
+                List<Items> items = new List<Items>();
+
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+
+                MySqlDataReader ItemsData = Connection.MySqlQuery("SELECT * FROM Shop_Toshmatovv.items ORDER BY `Name`;", MySqlConnection);
+
+                while (ItemsData.Read())
+                {
+                    items.Add(new Items()
+                    {
+                        Id = ItemsData.IsDBNull(0) ? -1 : ItemsData.GetInt32(0),
+                        Name = ItemsData.IsDBNull(1) ? "" : ItemsData.GetString(1),
+                        Description = ItemsData.IsDBNull(2) ? "" : ItemsData.GetString(2),
+                        Img = ItemsData.IsDBNull(3) ? "" : ItemsData.GetString(3),
+                        Price = ItemsData.IsDBNull(4) ? -1 : ItemsData.GetInt32(4),
+                        Categorys = ItemsData.IsDBNull(5) ? null : Categories.Where(x => x.Id == ItemsData.GetInt32(5)).First()
+                    });
+                }
+
+                MySqlConnection.Close();
+
+                return items;
+            }
+        }
+    }
+}

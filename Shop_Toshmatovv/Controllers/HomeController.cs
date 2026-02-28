79f8267bc04f185bcdc77d016f2a700d120eac42
Tashikv <vozhakovatasha1@gmail.com>
2026-02-28 09:20:38 +0500

    Разработанное перенаправление на страницу с предметами. Тошматов

---
+++ Shop_Toshmatovv/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop_Toshmatovv.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RedirectResult Index()
+        {
+            return Redirect("/Items/List");
+        }
+    }
+}
